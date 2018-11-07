a43ce9419553d131b7b792dc9ba9c3809559413e
samuelpereira7 <samuelpereira@gec.inatel.br>
2018-11-06 22:10:41 -0200

    Murmur3 hashing algorithm included.

--- kernel/core/hash.c
+++ kernel/core/hash.c
@@ -15,64 +15,140 @@
  */
 
 #include "hash.h"
-
-#ifndef GET_16_BITS
-#define GET_16_BITS( d ) 	((((portULONG)(((const portUCHAR*)(d))[1])) << 8) \
-							+ (portULONG)(((const portUCHAR*)(d))[0]))
-#endif
-
-//
-// super hash function by Paul Hsieh
-//
+#include <stdint.h>
 
 portHASH_TYPE xHash_calculateHash( const portCHAR* pcBuffer, portUINTEGER uiLength )
 {
-	portULONG ulHash = uiLength;
+	portHASH_TYPE xHash = 0;
 	portULONG ulTmp;
 	portINTEGER iRem;
 
+
     if (uiLength <= 0 || pcBuffer == NULL) return 0;
 
-    iRem = uiLength & 3;
-    uiLength >>= 2;
+    MurmurHash3_x86_32( pcBuffer, uiLength, 42, &xHash );
 
-    /* Main loop */
-    for (;uiLength > 0; uiLength--)
-    {
-        ulHash  += GET_16_BITS( pcBuffer );
-        ulTmp    = ( GET_16_BITS( pcBuffer + 2 ) << 11 ) ^ ulHash;
-        ulHash   = ( ulHash << 16 ) ^ ulTmp;
-        pcBuffer  += 2 * sizeof( portINTEGER );
-        ulHash  += ulHash >> 11;
-    }
+    return xHash;
+}
 
-    /* Handle end cases */
-    switch( iRem )
-    {
-        case 3:
-        	ulHash += GET_16_BITS( pcBuffer );
-			ulHash ^= ulHash << 16;
-			ulHash ^= pcBuffer[ sizeof( portINTEGER ) ] << 18;
-			ulHash += ulHash >> 11;
-			break;
-        case 2:
-        	ulHash += GET_16_BITS( pcBuffer );
-			ulHash ^= ulHash << 11;
-			ulHash += ulHash >> 17;
-			break;
-        case 1:
-        	ulHash += *pcBuffer;
-			ulHash ^= ulHash << 10;
-			ulHash += ulHash >> 1;
-    }
+//-----------------------------------------------------------------------------
+// MurmurHash3 was written by Austin Appleby, and is placed in the public
+// domain. The author hereby disclaims copyright to this source code.
 
-    /* Force "avalanching" of final 127 bits */
-    ulHash ^= ulHash << 3;
-    ulHash += ulHash >> 5;
-    ulHash ^= ulHash << 4;
-    ulHash += ulHash >> 17;
-    ulHash ^= ulHash << 25;
-    ulHash += ulHash >> 6;
+// Note - The x86 and x64 versions do _not_ produce the same results, as the
+// algorithms are optimized for their respective platforms. You can still
+// compile and run any of them on any platform, but your performance with the
+// non-native version will be less than optimal.
 
-    return ulHash;
+//-----------------------------------------------------------------------------
+// Platform-specific functions and macros
+
+#ifdef __GNUC__
+#define FORCE_INLINE __attribute__((always_inline)) inline
+#else
+#define FORCE_INLINE inline
+#endif
+
+static FORCE_INLINE uint32_t rotl32 ( uint32_t x, int8_t r )
+{
+  return (x << r) | (x >> (32 - r));
+}
+
+static FORCE_INLINE uint64_t rotl64 ( uint64_t x, int8_t r )
+{
+  return (x << r) | (x >> (64 - r));
+}
+
+#define	ROTL32(x,y)	rotl32(x,y)
+#define ROTL64(x,y)	rotl64(x,y)
+
+#define BIG_CONSTANT(x) (x##LLU)
+
+//-----------------------------------------------------------------------------
+// Block read - if your platform needs to do endian-swapping or can only
+// handle aligned reads, do the conversion here
+
+#define getblock(p, i) (p[i])
+
+//-----------------------------------------------------------------------------
+// Finalization mix - force all bits of a hash block to avalanche
+
+static FORCE_INLINE uint32_t fmix32 ( uint32_t h )
+{
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
+}
+
+//----------
+
+static FORCE_INLINE uint64_t fmix64 ( uint64_t k )
+{
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xff51afd7ed558ccd);
+  k ^= k >> 33;
+  k *= BIG_CONSTANT(0xc4ceb9fe1a85ec53);
+  k ^= k >> 33;
+
+  return k;
+}
+
+//-----------------------------------------------------------------------------
+
+void MurmurHash3_x86_32 ( const void * key, int len, uint32_t seed, void * out )
+{
+  const uint8_t * data = (const uint8_t*)key;
+  const int nblocks = len / 4;
+  int i;
+
+  uint32_t h1 = seed;
+
+  uint32_t c1 = 0xcc9e2d51;
+  uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
+
+  for(i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock(blocks,i);
+
+    k1 *= c1;
+    k1 = ROTL32(k1,15);
+    k1 *= c2;
+
+    h1 ^= k1;
+    h1 = ROTL32(h1,13);
+    h1 = h1*5+0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3)
+  {
+  case 3: k1 ^= tail[2] << 16;
+  case 2: k1 ^= tail[1] << 8;
+  case 1: k1 ^= tail[0];
+          k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+
+  h1 = fmix32(h1);
+
+  *(uint32_t*)out = h1;
 }
